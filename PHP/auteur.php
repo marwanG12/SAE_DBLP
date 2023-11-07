bd5e77943876b7dd605f80ba163c168ad07e9d37
marwanG12 <101125734+marwanG12@users.noreply.github.com>
2023-11-07 17:04:41 +0100

    details

--- PHP/auteur.php
+++ PHP/auteur.php
@@ -1,16 +1,21 @@
 <?php
 
 
-header("Access-Control-Allow-Origin: http://s5cdblp");
+header("Access-Control-Allow-Origin: http://saedblpgit");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
-$dbname = 'mghrairi';
+/*$dbname = 'mghrairi';
 $user = 'mghrairi';
 $password = 'lenouveauMDP';
 $host = 'database-etudiants.iut.univ-paris8.fr';
-$port = '5432';
+$port = '5432';*/
 
+$dbname = 'njouini';
+$user = 'njouini';
+$password = 'Noufnouf-78800';
+$host = 'database-etudiants.iut.univ-paris8.fr';
+$port = '5432';
 // Tentez de vous connecter à la base de données  
 $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
 
--- PHP/connexion.php
+++ PHP/connexion.php
@@ -1,13 +1,19 @@
 <?php
 
-header("Access-Control-Allow-Origin: http://s5cdblp");
+header("Access-Control-Allow-Origin: http://saedblpgit");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 // Informations de connexion à la base de données
-$dbname = 'mghrairi';
+/*$dbname = 'mghrairi';
 $user = 'mghrairi';
 $password = 'lenouveauMDP';
+$host = 'database-etudiants.iut.univ-paris8.fr';
+$port = '5432';*/
+
+$dbname = 'njouini';
+$user = 'njouini';
+$password = 'Noufnouf-78800';
 $host = 'database-etudiants.iut.univ-paris8.fr';
 $port = '5432';
 
